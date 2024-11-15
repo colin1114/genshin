06218a690fb439e66179ca24fb7d68754fcdde80
colin1114 <qlk_genshin@163.com>
2024-11-15 19:51:59 +0800

    new file:   .vscode/launch.json 	new file:   .vscode/settings.json     new file:   .vscode/tasks.json 	new file:   KMP.pdf 	modified:   README.md 	new file:   Untitled-1.cpp

---
+++ Untitled-1.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define endl '\n'
+
+using namespace std;
+const int E = 1e6 + 5;
+string a, b;
+int main()
+{
+    cin >> a >> b;
+    ll len = a.length(), len1 = b.length();
+
+    return 0;
+}
