205a7f05038b579606084f5361548a6bc3e12a7d
colin1114 <qlk_genshin@163.com>
2024-11-18 12:52:05 +0800

    modified:   Untitled-5.cpp

--- Untitled-5.cpp
+++ Untitled-5.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 
 string s;
-
+ll a[E];
 int main()
 {
     // freopen("1.txt", "r", stdin);
@@ -12,25 +12,30 @@ int main()
     ios::sync_with_stdio(0);
     cin.tie(nullptr);
 
-    while (cin >> s && s != ".")
-    {
-        int n = s.size();
-        vector<int> nxt(n + 1);
-        nxt[0] = -1;
-        int i = 0, j = -1;
-        while (i < s.size())
-        {
-            if (j == -1 || s[i] == s[j])
-            {
-                i++;
-                j++;
-                nxt[i] = j;
-            }
-            else
-                j = nxt[j];
-        }
-        cout << n / (n - nxt[n]) << endl;
-    }
-
+    // while (cin >> s && s != ".")
+    // {
+    //     int n = s.size();
+    //     vector<int> nxt(n + 1);
+    //     nxt[0] = -1;
+    //     int i = 0, j = -1;
+    //     while (i < s.size())
+    //     {
+    //         if (j == -1 || s[i] == s[j])
+    //         {
+    //             i++;
+    //             j++;
+    //             nxt[i] = j;
+    //         }
+    //         else
+    //             j = nxt[j];
+    //     }
+    //     cout << n / (n - nxt[n]) << endl;
+    // }
+    ll n;
+    a[1] = 1, a[2] = 1;
+    for (int i = 3; i <= 40; i++)
+        a[i] = a[i - 1] + a[i - 2];
+    cin >> n;
+    cout << a[n];
     return 0;
 }
