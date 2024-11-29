3e3d9d363570ef7c1274b7cd9e1112ddfb0bc192
colin1114 <qlk_genshin@163.com>
2024-11-29 20:27:43 +0800

    new file:   .cph/.Untitled-2.cpp_000d1f26ac8fb43446c394e09be23f15.prob 	modified:   Untitled-2.cpp

--- Untitled-2.cpp
+++ Untitled-2.cpp
@@ -4,8 +4,27 @@
 
 using namespace std;
 const int E = 1e6 + 5;
+ll t;
+ll x;
 int main()
 {
-
+    cin >> t;
+    while (t--)
+    {
+        cin >> x;
+        if (x <= 4)
+        {
+            cout << -1 << endl;
+            continue;
+        }
+        for (int i = 2; i <= x; i += 2)
+            if (i != 4)
+                cout << i << ' ';
+        cout << "4 5 ";
+        for (int i = 1; i <= x; i += 2)
+            if (i != 5)
+                cout << i << ' ';
+        cout << endl;
+    }
     return 0;
 }
