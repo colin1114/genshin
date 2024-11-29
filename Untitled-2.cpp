604783baecf46212d01051347695960c928a9795
colin1114 <qlk_genshin@163.com>
2024-11-29 20:18:53 +0800

    modified:   Untitled-2.cpp

--- Untitled-2.cpp
+++ Untitled-2.cpp
@@ -4,28 +4,8 @@
 
 using namespace std;
 const int E = 1e6 + 5;
-const int Base = 31;
-string a[E];
-string s;
-ll cnt;
-ll hsh[E];
-ll base[E];
 int main()
 {
-    string x;
-    while (cin >> x)
-    {
-        if (x == ".")
-            break;
-        a[++cnt] = x;
-    }
-    cin >> s;
-    ll len = s.length();
-    base[0] = 1;
-    for (int i = 1; i <= len; i++)
-        base[i] = base[i - 1] * 31 + s[i - 1];
-    for (int i = 1; i <= len; i++)
-        hsh[i] = hsh[i - 1] * 31 + base[i];
-    for (int i = 0; i < len; i++)
-        return 0;
+
+    return 0;
 }
