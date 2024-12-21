b8547052e66bb34a32b5ef93a4f164dde136f1ef
Your Name <your_email@example.com>
2024-12-21 21:29:09 +0800

    modified:   E_Snowflake_Tree.cpp

--- E_Snowflake_Tree.cpp
+++ E_Snowflake_Tree.cpp
@@ -1,57 +1,42 @@
 #include <bits/stdc++.h>
-using namespace std;
+#define ll long long
+#define endl '\n'
 
+using namespace std;
+const int E = 1e6 + 5;
+ll n;
+vector<ll> edge[E];
+ll dis[E];
 int main()
 {
-    ios::sync_with_stdio(false);
-    cin.tie(0);
-
-    int N;
-    cin >> N;
-    vector<vector<int>> adj(N + 1, vector<int>());
-    for (int i = 0; i < N - 1; ++i)
+    cin >> n;
+    for (int i = 1; i < n; i++)
     {
-        int u, v;
+        ll u, v;
         cin >> u >> v;
-        adj[u].push_back(v);
-        adj[v].push_back(u);
+        edge[u].push_back(v);
+        edge[v].push_back(u);
     }
-    vector<int> degrees(N + 1, 0);
-    for (int u = 1; u <= N; ++u)
+    for (int i = 1; i <= n; i++)
+        dis[i] = edge[i].size();
+    ll ans = 0;
+    for (int u = 1; u <= n; u++)
     {
-        degrees[u] = adj[u].size();
-    }
-
-    long long global_max = 0;
-    for (int u = 1; u <= N; ++u)
-    {
-        int deg_u = degrees[u];
-        if (deg_u == 0)
+        ll x = dis[u];
+        if (x == 0)
             continue;
-        vector<int> neighbor_degrees;
-        for (auto &v : adj[u])
+        vector<ll> a;
+        for (auto v : edge[u])
+            a.push_back(dis[v]);
+        sort(a.begin(), a.end(), greater<ll>());
+        for (int x = 1; x <= a.size(); x++)
         {
-            neighbor_degrees.push_back(degrees[v]);
-        }
-        sort(neighbor_degrees.begin(), neighbor_degrees.end(), [](const int a, const int b) -> bool
-             { return a > b; });
-        for (int x = 1; x <= neighbor_degrees.size(); ++x)
-        {
-            int d_x = neighbor_degrees[x - 1];
-            if (d_x >= 2)
-            {
-                // y = d_x -1 >=1
-                long long size_candidate = 1LL + (long long)x * d_x;
-                if (size_candidate > global_max)
-                {
-                    global_max = size_candidate;
-                }
-            }
+            if (a[x - 1] >= 2)
+                ans = max(ans, 1 + x * a[x - 1]);
             else
                 break;
         }
     }
-    long long deletions = (long long)N - global_max;
-
-    cout << deletions;
+    cout << n - ans << endl;
+    return 0;
 }
