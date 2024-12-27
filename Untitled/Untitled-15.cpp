c129136923549ff0bdb357c9b47f11b554b0f40f
colin1114 <qlk_genshin@163.com>
2024-12-27 20:54:22 +0800

    .

--- Untitled/Untitled-15.cpp
+++ Untitled/Untitled-15.cpp
@@ -8,9 +8,31 @@
 
 using namespace std;
 const int E = 1e6 + 5;
-
+ll n, m, sum;
+ll a[E], s[E];
 int main()
 {
-
+    cin >> n >> m;
+    for (int i = 1; i <= n; i++)
+        cin >> a[i];
+    for (int i = 2; i <= n; i++)
+    {
+        ll x = a[i - 1], y;
+        if (a[i - 1] > a[i])
+            y = a[i] + m;
+        else
+            y = a[i];
+        sum += y - x;
+        if (x <= y - 2)
+            x += 2, s[x]++, s[y + 1] -= y - x + 2, s[y + 2] += y - x + 1;
+    }
+    for (int i = 2; i <= 1e6; i++)
+        s[i] += s[i - 1];
+    for (int i = 2; i <= 1e6; i++)
+        s[i] += s[i - 1];
+    ll ans = 1e18;
+    for (int i = 1; i <= m; i++)
+        ans = min(ans, sum - s[i] - s[i + m]);
+    cout << ans;
     return 0;
 }
