c5459d8703ed402a95962b266b2c6b9eca91aad8
colin1114 <qlk_genshin@163.com>
2024-12-27 19:13:19 +0800

    new file:   .cph/.Untitled-14.cpp_6e450a139b43bfdf37c5dfd868ffc0ce.prob 	modified:   Untitled-14.cpp

--- Untitled-14.cpp
+++ Untitled-14.cpp
@@ -7,20 +7,27 @@
 #define y second
 
 using namespace std;
-const int E = 1e6 + 5;
+const int E = 1e7 + 5;
 ll n, m;
 struct node
 {
     ll l, r, a, b;
 } a[E];
+ll sum[E];
+ll s[E];
 int main()
 {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
     cin >> n >> m;
     for (int i = 1; i <= m; i++)
         cin >> a[i].l >> a[i].r >> a[i].a >> a[i].b;
     for (int i = 1; i <= m; i++)
     {
-        
+        ll x = (a[i].b - a[i].a) / (a[i].r - a[i].l);
+        s[a[i].r] += a[i].a;
+        s[a[i].l + 1] -= x - a[i].a;
     }
-        return 0;
+    return 0;
 }
