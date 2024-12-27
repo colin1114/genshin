cea4369a54b978afb330f67030aff495146f3c74
colin1114 <qlk_genshin@163.com>
2024-12-27 19:30:24 +0800

    modified:   Untitled-14.cpp 	new file:   hack.in 	new file:   hack.out 	modified:   make_in.cpp

--- Untitled-14.cpp
+++ Untitled-14.cpp
@@ -17,6 +17,8 @@ ll sum[E];
 ll s[E];
 int main()
 {
+    // freopen("hack.in", "r", stdin);
+    // freopen("hack.out", "w", stdout);
     ios::sync_with_stdio(0);
     cin.tie(0);
     cout.tie(0);
@@ -26,8 +28,21 @@ int main()
     for (int i = 1; i <= m; i++)
     {
         ll x = (a[i].b - a[i].a) / (a[i].r - a[i].l);
-        s[a[i].r] += a[i].a;
-        s[a[i].l + 1] -= x - a[i].a;
+        s[a[i].l] += a[i].a;
+        s[a[i].l + 1] += x - a[i].a;
+        s[a[i].r + 1] -= x + a[i].b;
+        s[a[i].r + 2] += a[i].b;
     }
+    // ll k = 0;
+    // for (int i = 1; i <= n; i++)
+    //     k += s[i], sum[i] = k;
+    for (int i = 1; i <= n; i++)
+        s[i] += s[i - 1];
+    for (int i = 1; i <= n; i++)
+        s[i] += s[i - 1];
+    ll ans = 0, max1 = 0;
+    for (int i = 1; i <= n; i++)
+        ans ^= s[i], max1 = max(max1, s[i]);
+    cout << ans << ' ' << max1;
     return 0;
 }
--- make_in.cpp
+++ make_in.cpp
@@ -10,8 +10,8 @@ int main()
     freopen("hack.in", "w", stdout);
     ios::sync_with_stdio(0);
     cout.tie(0);
-    cout << 450 << ' ' << 1000 << endl;
-    for (int i = 1; i <= 450; i++)
-        cout << "1 1 500 500\n";
+    cout << 100000 << ' ' << 300000 << endl;
+    for (int i = 1; i <= 300000; i++)
+        cout << "1 100000 1 100000\n";
     return 0;
 }
