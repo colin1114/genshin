d1d43f1dfb38e2dd16cb7896dc40b41dccf86ccd
colin1114 <qlk_genshin@163.com>
2025-01-23 08:51:14 +0800

    .

---
+++ Luogu_P_4560.cpp
@@ -0,0 +1,105 @@
+// SGT 区间修改区间查询模板
+// update on 2025-1-22.
+// lazy = min.
+// lazy1 = max.
+
+#include <bits/stdc++.h>
+#define ll long long
+#define endl '\n'
+
+using namespace std;
+const int E = 1e6 + 5;
+struct node
+{
+    ll d, lazy, lazy1;
+} ns[E * 4];
+ll n, m;
+
+// inline void update(ll rt)
+// {
+//     ns[rt].d = ns[rt * 2].d + ns[rt * 2 + 1].d;
+// }
+// inline void build(ll rt, ll cl, ll cr)
+// {
+//     ns[rt].d = 0, ns[rt].lazy = 0;
+//     if (cl == cr)
+//     {
+//         // ns[rt].d = ns[cl];
+//         return;
+//     }
+//     ll mid = (cl + cr) >> 1;
+//     build(rt * 2, cl, mid);
+//     build(rt * 2 + 1, mid + 1, cr);
+//     update(rt);
+// }
+
+inline void fx(ll rt, ll k)
+{
+    ns[rt].lazy = min(ns[rt].lazy, k);
+    ns[rt].lazy1 = max(ns[rt].lazy1, k);
+}
+
+inline void fy(ll rt, ll k)
+{
+    ns[rt].lazy = max(ns[rt].lazy, k);
+    ns[rt].lazy1 = min(ns[rt].lazy1, k);
+}
+
+inline void push_down(ll rt, ll l, ll r)
+{
+    fx(rt * 2, ns[rt].lazy);
+    fx(rt * 2 + 1, ns[rt].lazy);
+    fy(rt * 2, ns[rt].lazy1);
+    fy(rt * 2 + 1, ns[rt].lazy1);
+    ns[rt].lazy1 = 0;
+    ns[rt].lazy = 1e18;
+}
+
+inline void query(ll rt, ll l, ll r)
+{
+    if (l == r)
+    {
+        cout << ns[rt].lazy << endl;
+        return;
+    }
+    push_down(rt, l, r);
+    ll mid = (l + r) >> 1;
+    query(rt * 2, l, mid);
+    query(rt * 2 + 1, mid + 1, r);
+}
+
+inline void modily(ll rt, ll v, ll l, ll r, ll cl, ll cr, bool tags)
+{
+    if (cl >= l && cr <= r)
+    {
+        if (tags)
+            fy(rt, v);
+        else
+            fx(rt, v);
+        return;
+    }
+    push_down(rt, cl, cr);
+    ll mid = (cl + cr) >> 1;
+    if (l <= mid)
+        modily(rt * 2, v, l, r, cl, mid, tags);
+    if (r > mid)
+        modily(rt * 2 + 1, v, l, r, mid + 1, cr, tags);
+}
+int main()
+{
+    cin >> n >> m;
+    // build(1, 1, n);
+    for (int i = 0; i <= 1e6; i++)
+        ns[i].lazy = 1e18;
+    for (int i = 1; i <= m; i++)
+    {
+        ll op, l, r, h;
+        cin >> op >> l >> r >> h;
+        if (op == 1)
+            modily(1, h, l - 1, r - 1, 0, n - 1, 0);
+        else if (op == 2)
+            modily(1, h, l - 1, r - 1, 0, n - 1, 1);
+    }
+    query(1, 0, n - 1);
+    return 0;
+}
--- tmp.cpp
+++ tmp.cpp
@@ -1,93 +1,91 @@
+// SGT 区间修改区间查询模板
+// update on 2025-1-22.
+
 #include <bits/stdc++.h>
-#define endl '\n'
-
 #define ll long long
-
-#define PII pair<ll, ll>
-
-#define x first
-
-#define y second
+#define endl '\n'
 
 using namespace std;
 const int E = 1e6 + 5;
-ll n, m;
-ll a[E];
 struct node
 {
-    ll l, r, s, data;
-} tree[E << 2];
-inline node merge(node u, node v)
+    ll d, lazy;
+} ns[E * 4];
+ll n, m;
+inline void update(ll rt)
 {
-    node ans;
-    ans.s = u.s + v.s;
-    ans.l = max(u.l, u.s + v.l);
-    ans.r = max(v.r, v.s + u.r);
-    ans.data = max(max(u.data, v.data), u.r + v.l);
-    return ans;
+    ns[rt].d = ns[rt * 2].d + ns[rt * 2 + 1].d;
 }
-inline void build(ll l, ll r, ll id)
+inline void build(ll rt, ll cl, ll cr)
 {
-    tree[id].l = tree[id].r = tree[id].s = tree[id].data = 0;
-    if (l == r)
+    ns[rt].d = 0, ns[rt].lazy = 0;
+    if (cl == cr)
     {
-        tree[id].l = tree[id].r = tree[id].s = tree[id].data = a[l];
+        // ns[rt].d = ns[cl];
         return;
     }
-    ll mid = (l + r) >> 1;
-    build(l, mid, id << 1);
-    build(mid + 1, r, id << 1 | 1);
-    tree[id] = merge(tree[id << 1], tree[id << 1 | 1]);
+    ll mid = (cl + cr) >> 1;
+    build(rt * 2, cl, mid);
+    build(rt * 2 + 1, mid + 1, cr);
+    update(rt);
 }
-inline void modify(ll x, ll l, ll r)
+inline void push_down(ll rt, ll l, ll r)
 {
-    if (l == r)
+    ll mid = l + r >> 1;
+    ns[rt * 2].d += ns[rt].lazy * (mid - l + 1);
+    ns[rt * 2].lazy += ns[rt].lazy;
+    ns[rt * 2 + 1].d += ns[rt].lazy * (r - mid);
+    ns[rt * 2 + 1].lazy += ns[rt].lazy;
+    ns[rt].lazy = 0;
+}
+
+inline ll query(ll rt, ll l, ll r, ll cl, ll cr)
+{
+    if (cl >= l && cr <= r)
+        return ns[rt].d;
+    ll res = 0;
+    push_down(rt, cl, cr);
+    ll mid = (cl + cr) >> 1;
+    if (l <= mid)
+        res += query(rt * 2, l, r, cl, mid);
+    if (r > mid)
+        res += query(rt * 2 + 1, l, r, mid + 1, cr);
+    return res;
+}
+
+inline void modily(ll rt, ll v, ll l, ll r, ll cl, ll cr)
+{
+    if (cl >= l && cr <= r)
     {
-        tree[x].l = tree[x].r = tree[x].s = tree[x].data = a[l];
+        ns[rt].lazy += v;
+        ns[rt].d += v * (cr - cl + 1);
         return;
     }
-    ll mid = (l + r) >> 1;
-    modify(x << 1, l, mid);
-    modify(x << 1 | 1, mid + 1, r);
-    tree[x] = merge(tree[x << 1], tree[x << 1 | 1]);
+    push_down(rt, cl, cr);
+    ll mid = (cl + cr) >> 1;
+    if (l <= mid)
+        modily(rt * 2, v, l, r, cl, mid);
+    if (r > mid)
+        modily(rt * 2 + 1, v, l, r, mid + 1, cr);
+    ns[rt].d = (ns[rt * 2].d + ns[rt * 2 + 1].d);
 }
-inline node query(ll x, ll l, ll r, ll u, ll v)
-{
-    if (u <= l && r <= v)
-        return tree[x];
-    ll mid = (l + r) >> 1;
-    node p, q;
-    if (u <= mid)
-        p = query(x << 1, l, mid, u, v);
-    if (v > mid)
-        q = query(x << 1 | 1, mid + 1, r, u, v);
-    if (v <= mid)
-        return p;
-    if (u > mid)
-        return q;
-    return merge(p, q);
-}
+
 int main()
 {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    cout.tie(0);
-    cin >> n;
-    for (int i = 1; i <= n; i++)
-        cin >> a[i];
-    build(1, n, 1);
-    cin >> m;
-    while (m--)
+    cin >> n >> m;
+    build(1, 1, n);
+    for (int i = 1; i <= m; i++)
     {
-        ll op, x, y;
-        cin >> op >> x >> y;
+        ll op, l, r, h;
+        cin >> op >> l >> r >> h;
         if (op == 1)
         {
-            a[x] = y;
-            modify(1, 1, n);
+
         }
         else
-            cout << query(1, 1, n, x, y).data << endl;
+        {
+            
+        }
     }
     return 0;
 }
