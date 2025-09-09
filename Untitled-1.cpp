cef6de869ed874f50c39fee53ff1cbcce0fc124b
colin1114 <qlk_genshin@163.com>
2025-09-09 17:07:39 +0800

    untitled-1 changed

--- Untitled-1.cpp
+++ Untitled-1.cpp
@@ -3,52 +3,300 @@
 #define endl '\n'
 
 using namespace std;
-const int E = 1e6 + 5;
-ll n, cnt;
-ll bit[E];
-struct node
+ll n, m, k;
+struct opera
 {
-    ll a, b, c;
+    ll a[105];
+    opera() 
+	{
+        fill(a, a + 105, 0);
+    }
+    opera operator + (opera &s) const
+	{
+        opera c;
+        c.a[0] = max(a[0], s.a[0]);
+        for (int i = 1; i <= c.a[0]; i++) 
+		{
+            c.a[i] += a[i] + s.a[i];
+            c.a[i + 1] += c.a[i] / 10, c.a[i] %= 10;
+        }
+        if (c.a[c.a[0]]) 
+			c.a[0]++;
+        return c;
+    }
+    opera operator * (opera &s) const
+	{
+        opera c;
+        c.a[0] = a[0] + s.a[0] - 1;
+        for (int i = 1; i <= a[0]; i++) 
+			for (int j = 1; j <= s.a[0]; j++) 
+			{
+	            ll x = i + j - 1;
+	            c.a[x] += a[i] * s.a[j];
+	            c.a[x + 1] += c.a[x] / 10;
+	            c.a[x] -= (c.a[x] / 10) * 10;
+	        }
+        while (c.a[c.a[0] + 1]) 
+			c.a[++c.a[0] + 1] += c.a[c.a[0]] / 10, c.a[c.a[0]] %= 10;
+        
+        return c;
+    }
+    bool operator >(opera &s) const
+	{
+        if (a[0] > s.a[0]) 
+			return 1;
+        if (a[0] < s.a[0]) 
+			return 0;
+        for (int i = a[0]; i >= 1; i--) 
+		{
+            if (a[i] > s.a[i]) 
+				return 1;
+            if (a[i] < s.a[i]) 
+				return 0;
+        }
+        return 0;
+    }
+    void print() 
+	{
+        for (int i = a[0]; i >= 1; i--) 
+			cout << (ll)a[i];
+        cout << endl;
+    }
 };
-inline void update(ll x, ll v) 
+struct node 
 {
-    for (ll i = x; i <= n; i += i & -i)
-        bit[i] = min(bit[i], v);
-}
-inline ll query(ll x) 
+    vector<ll> v;
+    opera x;    
+    bool flag;     
+};
+inline opera calc(const vector<ll>& v) 
 {
-    ll res = 1e18;
-    for (ll i = x; i > 0; i -= i & -i)
-        res = min(res, bit[i]);
-    return res;
+    oprea ans;
+    change(ans, 1);
+    for (size_t i = 0; i < v.size(); i++) 
+    {
+        opera o;
+        change(o, 0);
+        ll t = v[i] - i;
+        if (t <= 0) 
+            return o;
+        change(o, t);
+        ans *= o;
+    }
+    return ans;
 }
-int main(){
-    cin >> n;
-    vector<node> t(n + 1);
-    ll x;
-    for (int i = 1; i <= n; i++) {
-        cin >> x;
-        t[x - 1].a = i;
+inline bool cmp(const node& a, const node& b) 
+{
+    if (!a.flag && !b.flag) 
+        return 0;
+    if (a.flag && !b.flag) 
+        return 1;
+    if (!a.flag &&  b.flag) 
+        return 0;
+    return a.x > b.x;
+}
+int main() 
+{
+    cin >> n >> m >> k;
+    vector<ll> a(n + 1);
+    for (int i = 1; i <= n; i++) 
+        cin >> a[i];
+    vector<vector<node>> dp(n+1, vector<node>(m+1));
+    dp[0][0].flag = 1;
+    dp[0][0].v.clear();
+    dp[0][0].x = 1;
+    for (ll i = 1; i <= n; i++) 
+    {
+        dp[i][0] = dp[i - 1][0]; 
+        for (ll j = 1; j <= m; j++) 
+        {
+            dp[i][j] = dp[i-1][j];
+            ll s = max(0LL, i - k);
+            if (dp[s][j - 1].flag) 
+            {
+                node node_ = dp[s][j-1];
+                auto it = lower_bound(node_.v.begin(), node_.v.end(), a[i]);
+                node_.v.insert(it, a[i]);
+                node_.x = calc(node_.v);
+                node_.flag = 1;
+                if (cmp(node_, dp[i][j])) 
+                    dp[i][j] = move(node_);
+            }
+        }
     }
-    for (int i = 1; i <= n; i++) {
-        cin >> x;
-        t[x - 1].b = i;
-    }
-    for (int i = 1; i <= n; i++) {
-        cin >> x;
-        t[x - 1].c = i;
-    }
-    sort(t.begin(), t.begin() + n, [&](const node& x, const node& y) {
-        return x.a < y.a;
-    });
-    fill(bit + 1, bit + n + 1, 1e18);
-    ll ans = 0;
-    for (int i = 1; i <= n; i++) {
-        ll x = query(t[i].b);
-        if (x > t[i].c) 
-            ans++;
-        update(t[i].b + 1, t[i].c);
-    }
-    cout << ans << endl;
+    node ans = dp[n][0];
+    for (int i = 1; i <= m; i++)
+        if (cmp(dp[n][i], ans)) 
+            ans = dp[n][i];
+    cout << ans.x << endl;
     return 0;
 }
+
+/*
+#include <bits/stdc++.h>
+#define ll long long
+
+#define endl '\n'
+
+
+using namespace std;
+const int E = 1e6 + 5;
+ll n, m, k, flag = 1;
+ll a[E];
+bitset<E> vis;
+struct opera
+{
+    ll a[105];
+    opera() 
+	{
+        fill(a, a + 105, 0);
+    }
+    opera operator + (opera &s) const
+	{
+        opera c;
+        c.a[0] = max(a[0], s.a[0]);
+        for (int i = 1; i <= c.a[0]; i++) 
+		{
+            c.a[i] += a[i] + s.a[i];
+            c.a[i + 1] += c.a[i] / 10, c.a[i] %= 10;
+        }
+        if (c.a[c.a[0]]) 
+			c.a[0]++;
+        return c;
+    }
+    opera operator * (opera &s) const
+	{
+        opera c;
+        c.a[0] = a[0] + s.a[0] - 1;
+        for (int i = 1; i <= a[0]; i++) 
+			for (int j = 1; j <= s.a[0]; j++) 
+			{
+	            ll x = i + j - 1;
+	            c.a[x] += a[i] * s.a[j];
+	            c.a[x + 1] += c.a[x] / 10;
+	            c.a[x] -= (c.a[x] / 10) * 10;
+	        }
+        while (c.a[c.a[0] + 1]) 
+			c.a[++c.a[0] + 1] += c.a[c.a[0]] / 10, c.a[c.a[0]] %= 10;
+        
+        return c;
+    }
+    bool operator >(opera &s) const
+	{
+        if (a[0] > s.a[0]) 
+			return 1;
+        if (a[0] < s.a[0]) 
+			return 0;
+        for (int i = a[0]; i >= 1; i--) 
+		{
+            if (a[i] > s.a[i]) 
+				return 1;
+            if (a[i] < s.a[i]) 
+				return 0;
+        }
+        return 0;
+    }
+    void print() 
+	{
+        for (int i = a[0]; i >= 1; i--) 
+			cout << (ll)a[i];
+        cout << endl;
+    }
+};
+opera ans;
+inline void change(opera &x, ll s) 
+{
+    while (s) 
+	{
+        x.a[++x.a[0]] = s % 10;
+        s /= 10;
+    }
+}
+inline void dfs(ll x, ll stp)
+{
+//	cerr << x << ' ' << stp << ' ' << pre << endl;
+	if (x > n)
+	{
+		if (stp > m)
+			return;
+//		for (int i = 1; i <= n; i++)
+//			cerr << vis[i] << ' ';
+//		cerr << endl;
+		vector<ll> p;
+		for (int i = 1; i <= n; i++)
+			if (vis[i])
+				p.push_back(i);
+		ll siz = p.size();
+		for (int i = 0; i < siz - 1; i++)
+			if (llabs(p[i] - p[i + 1]) < k)
+				return;
+		ll cnt = 0;
+		vector<ll> q;
+		for (int i = 0; i < siz; i++)
+			q.push_back(a[p[i]]);
+		sort(q.begin(), q.end());
+		opera max1;
+		change(max1, 1);
+		for (int i = 0; i < siz; i++)
+		{
+			opera o;
+			change(o, q[i] - cnt);
+			max1 = o * max1, cnt++;
+		}
+		if (max1 > ans)
+			ans = max1;
+		return;
+	}
+	if (stp > m)
+		return;
+//	if (pre + k >= x || flag)
+//	{
+//		if (flag)
+//			flag = 0;
+	vis[x] = 1;
+	dfs(x + 1, stp + 1);
+//	}
+	vis[x] = 0;
+	dfs(x + 1, stp);
+}
+// inline string to_string_(__int128 x)
+// {
+// 	string s = "";
+// 	while (x)
+// 	{
+// 		s += x % 10 + '0';
+// 		x /= 10;
+// 	}
+// 	return s;
+// }
+int main()
+{
+	freopen("ball.in", "r", stdin);
+	freopen("ball.out", "w", stdout);
+	change(ans, 0);
+	cin >> n >> m >> k;
+	for (int i = 1; i <= n; i++)
+		cin >> a[i];
+	if (k == 1)
+	{
+		sort(a + 1, a + 1 + n);
+		vector<ll> p;
+		for (int i = n - m + 1; i <= n; i++)
+			p.push_back(a[i]);
+		opera ans;
+		change(ans, 1);
+		ll cnt = 0;
+		for (int i = 0; i < p.size(); i++)
+		{
+			opera o;
+			change(o, p[i] - cnt);
+			ans = ans * o, cnt++;
+		}
+		ans.print();
+		return 0;
+	}
+	dfs(1, 0);
+	ans.print();
+	return 0;
+}
+*/
