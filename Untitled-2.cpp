b3121937bf2a0f8e22f1d1ae0ea2cde477e4298a
colin1114 <qlk_genshin@163.com>
2025-09-13 17:04:11 +0800

    new file:   .cph-ng/Untitled-2.cpp.bin 	new file:   .cph-ng/Untitled-3.cpp.bin 	modified:   Untitled-2.cpp 	modified:   Untitled-3.cpp 	to devolop a project.

--- Untitled-2.cpp
+++ Untitled-2.cpp
@@ -1,17 +1,81 @@
 #include <bits/stdc++.h>
-#define endl '\n'
 #define ll long long
+#define db double
+#define uint unsigned int
+#define ull unsigned long long
+// #define int ll
+
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+
+#define endl "\n"
+#define all(x) x.begin(), x.end()
+#define all1(x) x.begin() + 1, x.end()
+
+// #define x first
+// #define y second
 
 using namespace std;
-const int E = 1e6 + 5;
-ll t, n;
-int main()
-{
-    cin >> t;
-    while (t--)
-    {
-        cin >> n;
-        
+
+const int inf = 2e9;
+const ll INF = 2e18;
+const int mod = 1e9 + 7;
+const db eps = 1e-7;
+const int maxn = 1e5 + 5;
+const int N = 1e7 + 5;
+
+void solve() {
+    string a, b;
+    cin >> a >> b;
+    vector<int> cnt1(26, 0), cnt2(26, 0);
+    for (auto v : a) cnt1[v - 'a'] += 1;
+    for (auto v : b) cnt2[v - 'a'] += 1;
+
+    vector<int> ans(26, 0);
+    int curr = 0, tot = 0;
+    for (int i = 0; i < 26; i++) if (!cnt2[i] && cnt1[i]) curr += 1, ans[i] = 1;
+
+    // for (int i = 0; i < 26; i++) cerr << cnt1[i] << ' ';
+    // cerr << endl;
+
+    if (curr <= 1) {
+        cout << -1 << endl;
+        return;
     }
+    int cnt = 0;
+    int p1 = -1, p2 = -1;
+    for (int i = 0; i < 26; i++) if (ans[i]) p1 = p2, p2 = i;
+    for (int i = 0; i < 26; i++) {
+        if (i == p1 || i == p2) {
+            if (cnt == 0)
+                for (int j = 1; j <= cnt1[i] - ((cnt1[i] & 1) ^ 1); j++) cout << (char)('a' + i);
+            else 
+                cout << (char)('a' + i);
+            cnt += 1;
+        }
+        else if (cnt == 0 || !ans[i]) {
+            for (int j = 1; j <= cnt1[i]; j++) cout << (char)('a' + i);
+            if (ans[i] && cnt1[i] % 2) cnt += 1;
+        }
+        else cout << (char)('a' + i), cnt += 1;
+        if (cnt == 2) break;
+    }
+    cout << endl;
+}
+
+int main() {
+    freopen("string.in", "r", stdin);
+    freopen("string.out", "w", stdout);
+
+    ios::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+
+    int T = 1;
+    cin >> T;
+
+    while (T--) {
+        solve();
+    }
+
     return 0;
 }
--- Untitled-3.cpp
+++ Untitled-3.cpp
@@ -4,30 +4,78 @@
 
 using namespace std;
 const int E = 1e6 + 5;
-ll t, n;
-ll a[E], sum[E];
+ll n, ans;
+ll a[E], dep[E], in[E], out[E];
+vector<ll> edge[E];
+inline void init(ll u, ll fa)
+{
+    dep[u] = dep[fa] + 1;
+    for (auto v : edge[u])
+        if (v != fa)
+            init(v, u);
+}
+inline void dfs(ll u, ll fa, ll tim, ll stp, ll life)
+{
+    // cerr << "id: " << u << ", pre: " << fa << ", time: " << tim << ", dep: " << dep[u] << ", step: " << stp << ", life: " << life << ", will " << (a[u] > 0 ? '-' : '+') << ' ' << 1 << " life" << endl;
+    if (dep[u] <= tim)
+    {
+        // cerr << "ok stp = " << stp << endl;
+        // cerr << "exit signel: 1\n";
+        // cerr << endl;
+        ans = max(ans, stp);
+        return;
+    }
+    life += a[u];
+    if (life <= 0)
+    {
+        ans = max(ans, stp);
+        // cerr << "ok stp = " << stp << endl;
+        // cerr << "exit signel: 2\n";
+        return;
+    }
+    for (auto v : edge[u])
+        dfs(v, u, tim + 1, stp + 1, life);
+}
 int main()
 {
-    freopen("array.in", "r", stdin);
-    freopen("array.out", "w", stdout);
+    freopen("vol.in", "r", stdin);
+    freopen("vol.out", "w", stdout);
     ios::sync_with_stdio(0);
     cin.tie(0);
     cout.tie(0);
-    cin >> t;
-    while (t--)
+    cin >> n;
+    for (int i = 1; i <= n; i++)
+        cin >> a[i];
+    for (int i = 1; i < n; i++)
     {
-        fill(sum, sum + 2 + n, 0);
-        cin >> n;
-        for (int i = 1; i <= n; i++)
-        {
-            cin >> a[i];
-            sum[i] = sum[i - 1] + a[i];
-        }
-        ll max1 = sum[n];
-        for (int i = 1; i <= n; i++)
-            if (sum[n] - sum[i] > 0)
-                max1 += sum[n] - sum[i];
-        cout << max1 << endl;
+        ll u, v;
+        cin >> u >> v;
+        edge[u].push_back(v);
+        in[u]++;
+        out[v]++;
+        edge[v].push_back(u);
+    }
+    ll cnt = 0, cnt1 = 0;
+    for (int i = 1; i <= n; i++)
+    {
+        if (in[i] == 1)
+            cnt++;
+        if (out[i] == 1)
+            cnt1++;
+    }
+    init(1, 0);
+    for (int i = 1; i <= n; i++)
+        cerr << --dep[i] << ' ';
+    if (cnt == n - 2 && cnt1 == n - 2)
+    {
+        
+    }
+    for (int i = 1; i <= n; i++)
+    {
+        // cerr << i << endl;
+        ans = 0;
+        dfs(i, 0, 0, 0, 1);
+        cout << ans << endl;
     }
     return 0;
 }
